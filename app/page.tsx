7b35bd7449f70014c1e9583fad6e50f05fe969b4
Ricky Adriell <53133228+rickyadriell@users.noreply.github.com>
2024-12-30 16:39:03 +0000

    add navigation links to personal website and social media

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import profile from "./icon.png"
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import fireworks from './fireworks.json';
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +11,23 @@ export default function Home() {
       <div className="flex flex-col items-center gap-10">
         <Image src={profile} alt='R' className="w-[300px] h-[300px]" />
         <h1 className="text-6xl md:text-8xl lg:text-9xl font-bold text-center">Happy New Year!</h1>
+        <div id="links" className="flex gap-5 z-50">
+          <Link href="https://rickyadriell.com" className="font-bold">
+            Home
+          </Link>
+          <span>|</span>
+          <Link href="https://github.com/rickyadriell" className="font-bold">
+            GitHub
+          </Link>
+          <span>|</span>
+          <Link href="https://linkedin.com/in/rickyadriell/" className="font-bold">
+            Linkedin
+          </Link>
+          <span>|</span>
+          <Link href="https://instagram.com/codecplyre" className="font-bold">
+            Instagram
+          </Link>
+        </div>
       </div>
       <div className="flex gap-10 absolute bottom-0 h-3/4">
         {[...Array(3)].map((_, i) => (
